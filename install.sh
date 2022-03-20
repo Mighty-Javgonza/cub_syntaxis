1b0b9279b8e447950a99f5fd3d96d6aeffc6d14b
Javier Gonzalez nicora <javgonza@c3r2s2.42madrid.com>
2022-03-20 20:19:56 +0100

    Updated install and uninstall to check for success

--- install.sh
+++ install.sh
@@ -1,3 +1,14 @@
+
+REPO_DIR=$(dirname $0)
+
 mkdir -p ~/.vim/plugin/syntaxis
-cp cub_syntaxis.vim ~/.vim/plugin/syntaxis/cub_syntaxis.vim
-echo Install successful
+cp "$REPO_DIR/cub_syntaxis.vim" ~/.vim/plugin/syntaxis/cub_syntaxis.vim
+
+DIFFERENCE=$(diff "$REPO_DIR/cub_syntaxis.vim" ~/.vim/plugin/syntaxis/cub_syntaxis.vim)
+
+if [ $? == 0 ] && [ "$DIFFERENCE" == "" ]
+then
+	echo Install successful
+else
+	echo Install failed
+fi
--- uninstall.sh
+++ uninstall.sh
@@ -1,1 +1,8 @@
 rm -i ~/.vim/plugin/syntaxis/cub_syntaxis.vim
+
+if [ $? == 0 ]
+then
+	echo Uninstall successful
+else
+	echo Uninstall failed
+fi
