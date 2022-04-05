b4ed702f29d987582d19aecf43a91fb4919bba60
Javier Gonzalez nicora <javgonza@student.42madrid.com>
2022-04-05 18:38:10 +0200

    corrected typo on install script

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 FOLDER=$(dirname $0)
 
-read -p "Install custom colors? (If you choose no then colors will depend on your theme/colorscheme)[y/n]:" -n 1 -r
+read -p "Install custom colors? (If you choose no, then colors will depend on your theme/colorscheme)[y/n]:" -n 1 -r
 echo
 if [ $REPLY == 'Y' ] || [ $REPLY == 'y' ]
 then
